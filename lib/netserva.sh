d06946e8a1efc15d8297b479719435fd443df5a5
Mark Constable <markc@renta.net>
2025-04-07 13:09:27 +1000

    Normalized the head of all shell scripts

---
+++ lib/netserva.sh
@@ -0,0 +1,14 @@
+# Created: 20250604
+# Copyright (C) 1995-2025 Mark Constable <markc@renta.net> (AGPL-3.0)
+#
+# Global configuration settings for NetServa SH
+# These settings are intended to be read-only and shared across all scripts
+
+# Set the PATH to include system directories and NetServa bin
+PATH=/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/bin:~/.sh/bin
+
+# Default editor for the environment
+EDITOR=nano
+
+# Terminal color setting for the PS1 prompt
+COLOR=31
